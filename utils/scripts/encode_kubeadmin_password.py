b510eef8f95afe22e6a16897038d3e6e0f9680b6
William Caban <william.caban@redhat.com>
2020-03-12 12:29:20 -0400

    Fix hardcoded string with variable

--- utils/scripts/encode_kubeadmin_password.py
+++ utils/scripts/encode_kubeadmin_password.py
@@ -6,10 +6,10 @@ except ImportError:
     raise ImportError("Missing 'bcrypt' or 'base64' libraries ")
 
 # Password Format: 5char-5char-5char-5char
-MYPASSWORD = "a1b2c-3d4e5-f6g7h-8i9ab"
+MYPASSWORD = b"a1b2c-3d4e5-f6g7h-8i9ab"
 
 # Generate ascii encoded encrypted password with random salt
-encrypted_password = bcrypt.hashpw(b"a1b2c-3d4e5-f6g7h-8i9ab",bcrypt.gensalt())
+encrypted_password = bcrypt.hashpw(MYPASSWORD,bcrypt.gensalt())
 
 b64_password = base64.b64encode(encrypted_password)
 
